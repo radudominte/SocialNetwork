b859d41ee319ebfc677dfa74fa4b916bf0f9ea32
Radu Dominte <radudominte@imac-di-radu.lan>
2017-01-28 12:37:40 +0100

    Initial Commit

---
+++ SocialNetwork/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialNetwork
+//
+//  Created by Radu Dominte on 28/01/17.
+//  Copyright © 2017 Radu Dominte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
