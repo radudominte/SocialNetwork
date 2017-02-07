34ef5b8b065bc61980e960108ede26dae1aa7aa1
Radu Dominte <radudominte@iMac-di-Radu.lan>
2017-02-07 07:39:42 +0100

    completed firebase-users

--- SocialNetwork/SignInVC.swift
+++ SocialNetwork/SignInVC.swift
@@ -74,7 +74,8 @@ class SignInVC: UIViewController {
                 
                 if let user = user {
                     
-                    self.completeSignIn(id: user.uid)
+                    let userData = ["provider": credential.provider]
+                    self.completeSignIn(id: user.uid, userData: userData)
                     
                 }
             }
@@ -94,7 +95,8 @@ class SignInVC: UIViewController {
                     
                     if let user = user {
                         
-                        self.completeSignIn(id: (user.uid))
+                        let userData = ["provider": user.providerID]
+                        self.completeSignIn(id: user.uid, userData: userData)
                     }
                     
                 } else {
@@ -110,7 +112,8 @@ class SignInVC: UIViewController {
                             
                             if let user = user {
                                 
-                                self.completeSignIn(id: (user.uid))
+                                let userData = ["provider": user.providerID]
+                                self.completeSignIn(id: user.uid, userData: userData)
                             }
                         }
                     })
@@ -120,7 +123,9 @@ class SignInVC: UIViewController {
     }
     
     
-    func completeSignIn(id: String) {
+    func completeSignIn(id: String, userData: Dictionary<String, String>) {
+        
+        DataService.ds.createFirebaseDBUser(uid: id, userData: userData)
         
         let keychainResult = KeychainWrapper.standard.set(id, forKey: KEY_UID)
         
